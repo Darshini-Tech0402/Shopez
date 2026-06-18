a036674d1c0445798e7b73e9f8fc99cdd8274ea6
Darshini <scarletscar303@gmail.com>
2026-06-18 20:12:52 +0530

    Updated product display fixes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useProducts } from '../context/ProductContext';
 import ProductCard from '../components/ProductCard';
+import PRODUCTS from '../data/products';
 import './Home.css';
 
 const Home = () => {
